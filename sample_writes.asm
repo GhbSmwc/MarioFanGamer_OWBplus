490ab02cf37bebc3befbfcb1e9ad361b913ad525
GhbSmwc <GhbSmwc@users.noreply.github.com>
2019-03-18 15:30:02 -0400

    Updated for optimized loop.
    the loop to write tiles in the sample now handles both tile numbers and properties at the same time.

--- sample_writes.asm
+++ sample_writes.asm
@@ -9,24 +9,27 @@
 	
 	main:
 	
-	LDA #$22
-	;^Number tiles (digits 0-9) are located at page 1 at
-	;tiles $22 to $2B. If you are using a display of
-	;numbers that can change, after using the Hex->Dec
-	;to convert to decimal, you add each digit byte by
-	;$22 to convert them to overworld digits. An example is
-	;in [Overworld_Border_Plus.asm] under
-	;"if !Enable_Lives_Display" (I, GreenHammerBro added comments).
-	
 	LDX.b #(3-1)*2 ;Write 3 times for each 3 tile byte; indexes 0, 2, and 4 to write.
 	
-	.LoopTileNumb
-	STA !WritePosition,x
-	DEX #2
-	BPL .LoopTileNumb
+	.LoopTile
+	LDA #$22			;>See [1]
+	STA !WritePosition,x		;>Tile number [TTTTTTTT] byte
+	LDA.b #%00111001		;>See [2]
+	STA !WritePosition+$01,x	;>Tile properties [YXPCCCTT] byte
+	DEX #2				;>Decrease X by 2 (2 bytes per 8x8 tile)
+	BPL .LoopTile			;>If there are remaining tiles left, keep writing until X = $FE
+
+	;[1] Number tiles (digits 0-9) are located at page 1 at
+	;tiles $22 to $2B (hence the #$22 on the LDA). If you
+	;are using a display of numbers that can change, after
+	;using the Hex->Dec to convert to decimal, you add each
+	;digit byte by $22 to convert them to overworld digits.
+	;An example is in [Overworld_Border_Plus.asm] under
+	;"if !Enable_Lives_Display" (I, GreenHammerBro added comments).
 	
-	;       YXPCCCTT
-	LDA.b #%00111001
+	;[2] Tile Properties:
+	;        YXPCCCTT
+	;LDA.b #%00111001 (that in hex is #$39).
 	;^Bits [TT] is the page number, ranging from 0-3 (%00 to %11), the digit
 	;  tiles are located at page 1 (%01).
 	; Bits [CCC] is what palette to use, ranging from 0-7 (%000 to %111),
@@ -34,18 +37,11 @@
 	;  is considered the normal palette for numbers.
 	; Bits [P] is the priority bit, determines if this tile goes in front
 	;  of all layer 1 and 2 stuff. Recommended to have this set to 1 and
-	;  and not 0 if you don't want stuff like clouds and other layer 1 stuff
-	;  overlapping it.
+	;  and not 0 if you don't want stuff like sprites (clouds) and
+	;  layer 1 stuff overlapping it.
 	; Bits [X] is the X flip of the tile.
 	; Bits [Y] is the Y flip of the tile.
-	LDX.b #(3-1)*2 ;Write 3 times for each 3 tile properties byte (indexing is same as above)
-	
-	.LoopTileProps
-	STA !WritePosition+$01,x	;>Write at tile tile properties that are +1 byte after each tile number byte.
-	DEX #2
-	BPL .LoopTileProps
-	RTL
-;Another example: Write "TEST". Note: On most emulators, the top line of pixels are cutoff:
+;Another example: Write "TEST" without using loops. Note: On most emulators, the top line of pixels are cutoff:
 	
 	!WritePosition = $7FEC00 ;>Change to $41EC00 for SA-1
 	
